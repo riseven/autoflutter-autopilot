c6b6b803e02d95a05c466367f403723ed935bcf9
riseven <riseven@riseven.com>
2023-02-20 18:45:38 +0100

    Created high level logic and interfaces

--- autoflutter-autopilot/autoflutter-autopilot.ino
+++ autoflutter-autopilot/autoflutter-autopilot.ino
@@ -1,3 +1,35 @@
+#include "interfaces.h"
+
+State state;
+
+void read_radio(RadioData &radio_data)
+{
+}
+
+void failsafe()
+{
+}
+
+void actuators_from_radio(RadioData &radio_data, Actuators &actuators)
+{
+}
+
+void read_sensors(SensorData &sensor_data)
+{
+}
+
+void process_state(State &state, SensorData &sensor_data)
+{
+}
+
+void autopilot(State &state, Actuators &actuators)
+{
+}
+
+void send_actuators(Actuators &actuators)
+{
+}
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -5,5 +37,26 @@ void setup()
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  RadioData radio_data;
+  SensorData sensor_data;
+  Actuators actuators;
+
+  read_radio(radio_data);
+
+  if (radio_data.is_radio_on)
+  {
+    failsafe();
+    return;
+  }
+
+  if (!radio_data.is_auto_pilot_on)
+  {
+    actuators_from_radio(radio_data, actuators);
+    send_actuators(actuators);
+  }
+
+  read_sensors(sensor_data);
+  process_state(state, sensor_data);
+  autopilot(state, actuators);
+  send_actuators(actuators);
 }
---
+++ autoflutter-autopilot/interfaces.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class RadioData
+{
+public:
+  bool is_radio_on;
+  bool is_auto_pilot_on;
+  // And the rest of signals from radio
+  // ...
+};
+
+class SensorData
+{
+public:
+  // Whatever it is that we have sensors for
+};
+
+class State
+{
+public:
+  // Whatever
+};
+
+class Actuators
+{
+public:
+  // Whatever
+};
